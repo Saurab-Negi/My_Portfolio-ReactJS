745435b986a94eb5e09969b232bf2200eebb624d
Saurab-Negi <saurabnegi17@gmail.com>
2024-02-26 19:15:52 +0530

    Added Experience, Card and Services

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -2,22 +2,47 @@ import './Services.css'
 import HeartEmoji from "../../Images/heartemoji.png"
 import Glasses from "../../Images/glasses.png"
 import Humble  from "../../Images/humble.png"
+import Card from '../Card/Card'
+import Resume from './Resume.pdf'
 
 const Services = () => {
   return (
     <div className="services">
         {/* left side */}
-        <div className="s-details"></div>
+        <div className="s-details">
         <span>My</span>
         <span>Services</span>
         <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores aperiam omnis fugiat,<br></br>
             cum nostrum ea, quas saepe eligendi expedita placeat delectus exercitationem rem!
         </span>
-        <button className="button s-button">Resume</button>
-        <div className="blur s-blur"></div>
+        <a href={Resume} download><button className="button s-button">Resume</button></a>
+        <div className="blur s-blur1"></div>
+        </div>
+        
         {/* right side */}
         <div className="cards">
-            Right side
+            <div className='card-1'>
+              <Card
+              emoji={HeartEmoji}
+              heading={'Design'}
+              detail={"Figma, sketch, photoshop, Adobe"}
+              />
+            </div>
+            <div className='card-2'>
+              <Card
+              emoji={Glasses}
+              heading={'Developer'}
+              detail={"Html, Css, JavaScript, React"}
+              />
+            </div>
+            <div className='card-3'>
+              <Card
+              emoji={Humble}
+              heading={'UI/UX'}
+              detail={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+              />
+            </div>
+            <div className="blur s-blur2"></div>
         </div>
     </div>
   )
